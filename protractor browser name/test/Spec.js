5527a41cd89f2d86c1481ce9cb5f727056633eae
ashikh <ashee.vajra@gmail.com>
2016-06-19 19:01:09 +0530

    assignment done

--- protractor browser name/test/Spec.js
+++
@@ -1,6 +0,0 @@
-describe('Protractor test', function (){
-	it('first test', function(){
-		browser.get('http://www.google.com');
-		expect(browser.getTitle()).toEqual('Google');
-	});
-});
---
+++ protractor variable/test/Spec.js
@@ -0,0 +1,7 @@
+describe('Protractor test', function (){
+	var msg='Hello World';
+	it('second test', function(){
+		
+		expect(msg).toEqual('Hello World');
+	});
+});
