1b214a3b728a3ffb985b62989104afb6d6a9511e
ashikh <ashee.vajra@gmail.com>
2016-06-19 19:16:42 +0530

    assignment done

--- protractor key value bind/conf.js
+++
@@ -1,8 +0,0 @@
-exports.config={
-	framework:'jasmine',
-	seleniumAddress:'http://127.0.0.1:4444/wd/hub',
-	capabilities:{
-		browserName: 'chrome'
-	},
-	specs:['test/keySpec.js']
-}
--- protractor key value bind/test/keySpec.js
+++
@@ -1,9 +0,0 @@
-describe('testing key value',function(){
-it('must check key-value equal or not',function(){
-	browser.get('http://127.0.0.1:64771/index.html');
-	element(by.model('a')).sendKeys('45');
-	element(by.model('b')).sendKeys('77');
-	expect(element(by.binding('a')).getText()).toEqual('45');
-	expect(element(by.binding('b')).getText()).toEqual('77');
-});
-});
