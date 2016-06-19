671e65db200dd040a594bc1129d4089691104c41
ashikh <ashee.vajra@gmail.com>
2016-06-19 18:49:33 +0530

    assignment done

--- jasmine array/Jasmine Unit Testing/spec/arraySpec.js
+++
@@ -1,8 +0,0 @@
-describe('This suite is to check if a given value exist in array', function() {
-	var names=['avinash', 'sunder', 'naveen','ravi','raj']
-	it('This tests for values in array', function(){
-		expect(names).toContain('naveen');
-		expect(names).toContain('raj');
-	});
-	
-});
